dc8d3b86f4ab4d6edbde4ad31e2866085f760f2d
cboecking <chuck@chuboe.com>
2016-08-04 13:50:52 -0500

    added first project to test

--- go_install.sh
+++ go_install.sh
@@ -5,6 +5,7 @@ GO_OS=linux
 GO_ARCH=amd64
 GO_DIR=/usr/local/go/
 GO_TMP=/tmp/
+GO_TMP_PATH=$HOME/gowork/
 
 #check to see if go is already installed
 GO_EXISTS=$([ -d $GO_DIR ] && echo "Y" || echo "N")
@@ -21,14 +22,26 @@ else
 	cd $GO_TMP
 	sudo curl -O https://storage.googleapis.com/golang/go$GO_VERSION.$GO_OS-$GO_ARCH.tar.gz
 	sudo tar -C /usr/local -xzf go$GO_VERSION.$GO_OS-$GO_ARCH.tar.gz
+	cd
 fi
 
+#add go ccommands to your path
 sed -i "$ a\export PATH=\$PATH:$GO_DIR/bin" ~/.profile
 
-mkdir ~/gowork
-
-sed -i "$ a\export GOPATH=\$HOME/gowork" ~/.profile
+#create your workspace (GOPATH)
+mkdir $GO_TMP_PATH
+sed -i "$ a\export GOPATH=$GO_TMP_PATH" ~/.profile
 
 source ~/.profile
 
+#create and execute your first go project
+mkdir -p $GOPATH/src/deleteme/
+cd $GOPATH/src/deleteme/
+wget https://raw.githubusercontent.com/cboecking/golang-env-setup/master/deleteme.go
+go install deleteme
+$GOPATH/bin/deleteme
+
+#go to home directory
 cd
+
+echo "if the installation was successful, you should see a hello world message just above this line."
